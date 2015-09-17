526e439543811b52e400106fc0c9f3095d88ef7c
txoof <aaron.ciuffo@gmail.com>
2015-09-17 11:20:15 +0200

    update version number

--- automate_rsync.py
+++ automate_rsync.py
@@ -13,7 +13,7 @@ import re
 # handle commandline options
 import sys, getopt
 
-version='automate rsync over ssh: version 0.2'
+version='automate rsync over ssh: version 0.3'
 
 
 # locate executables in the path
