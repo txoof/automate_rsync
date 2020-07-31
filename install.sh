ff92d374edc09e8dee4eb5a3d6e3c7d78d0ff7be
txoof <aaron.ciuffo@gmail.com>
2020-07-31 11:08:06 +0200

    add buffer=1 to syncronize writes to log

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+install_path=/usr/local/bin
+executable=automate_rsync.py
+
+echo "This script will install automate_rsync in $install_path"
+echo "This likely requires super user permissions"
+
+if [ ! -d $install_path ]; then
+  echo "allow this script to make $install_path? -- ctrl-c to cancel"
+  sudo mkdir -p $install_path
+fi
+
+cp $executable $install_path/${executable:r}
