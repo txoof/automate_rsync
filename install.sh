eb1d4d21b568b507126091f5a9bf3be495bf7cd9
txoof <aaron.ciuffo@gmail.com>
2020-07-31 11:09:56 +0200

    fix hasbang

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 install_path=/usr/local/bin
 executable=automate_rsync.py
 
 echo "This script will install automate_rsync in $install_path"
-echo "This likely requires super user permissions"
 
 if [ ! -d $install_path ]; then
+  echo "This likely requires super user permissions"
   echo "allow this script to make $install_path? -- ctrl-c to cancel"
   sudo mkdir -p $install_path
 fi
