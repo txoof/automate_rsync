3f06c2e079d1d2c82d112f0a4a0734c90827fdf6
txoof <aaron.ciuffo@gmail.com>
2020-07-31 12:14:48 +0200

    fix line endings

--- remote_install/launchd_install.sh
+++ remote_install/launchd_install.sh
@@ -22,8 +22,10 @@ pushd $launch_agent_path
 curl -s -O $url
 popd
 launchctl load $launch_agent_path/$plist_file
-printf "\nautomate_rsync will now run every 1800 seconds (30 min)."
-printf "edit $launch_agent_path/$plist_file to change the interval"
-printf "it is critical that you have provided bash full disk access."
-printf "if you have not done this yet see: https://www.tech-otaku.com/mac/manually-granting-applications-full-disk-access-macos-catalina/"
+printf "\nautomate_rsync will now run every 1800 seconds (30 min).\n"
+printf "edit $launch_agent_path/$plist_file to change the interval\n"
+printf "############################################################\n"
+printf "it is critical that you have provided bash full disk access!\n"
+printf "############################################################\n"
+printf "if you have not done this yet see: https://www.tech-otaku.com/mac/manually-granting-applications-full-disk-access-macos-catalina/ \n"
 
